a1b2101d94ad7a82eae135a699a2581d9c761683
Anita Okologo <harneeetah@gmail.com>
2023-02-21 12:56:41 -0800

    putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - Prints _putchar
+ *
+ * Description: Prints _putchar followed by a newline
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+printf("_putchar,\n");
+	return (0);
+}
