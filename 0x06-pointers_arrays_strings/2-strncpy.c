b7f65146abfdaf1f3d7e7ff9bc66b75b853fbad7
Anita Okologo <harneeetah@gmail.com>
2023-03-01 15:49:11 -0800

    strncpy code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,14 @@
  * Return: A pointer to the resulting string dest.
  */
 
- char *_strncpy(char *dest, char *src, int n)
-           {
-               int index = 0;
+	char *_strncpy(char *dest, char *src, int n)
+	{
+		int index = 0;
 
-               for (index = 0; src[index] && index < n; index++)
-                   dest[index] = src[index];
-               for ( ; index < n; index++)
-                   dest[index] = '\0';
+		for (index = 0; src[index] && index < n; index++)
+			dest[index] = src[index];
+		for ( ; index < n; index++)
+			dest[index] = '\0';
 
-               return (dest);
-           }
+		return (dest);
+	}
