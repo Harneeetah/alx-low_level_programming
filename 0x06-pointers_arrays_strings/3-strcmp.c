db47ee542386e44e626bdf7ff3cc8ce26d998145
Anita Okologo <harneeetah@gmail.com>
2023-03-01 16:36:52 -0800

    addition

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,12 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: If str1 < str2, the negative difference of
- * the first unmatched characters.
- *         If s1 == s2, 0.
- *         If s1 > s2, the positive difference of
- *         the first unmatched characters.
+ * Return: If s1 <= s2, the negative difference of
+ * the first unmatched characters or zero if they have the same value.
+ * If s1 >= s2, the positive difference of
+ * the characters or zero if they have the same value.
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	if ((*s1 >= *s2) && (*s1 <= *s2))
